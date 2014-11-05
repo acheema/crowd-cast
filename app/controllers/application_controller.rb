543822d66651bf472a7f8a6c43310cbc9e9899d7
jesca <jesca116@gmail.com>
2014-11-05 14:18:13 -0800

    saving dashboard state view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,24 +6,34 @@ class ApplicationController < ActionController::Base
    #protect_from_forgery with: :exception
    #protect_from_forgery with: :null_session, if: Proc.new { |c| c.request.format.json? }
 
-   helper_method :current_user
+   helper_method :set_current_user
    before_filter :set_current_user
 
 private
 def set_current_user
    if cookies[:username]
     #if usertype ==2 just query the advertiser database....if they are both an advertiser and owner, then the information is in both databases
-    if cookies[:usertype] == "0" || cookies[:usertype] == "2"
+    if cookies[:usertype] == "0"
       @current_user ||= Advertiser.find_by(username: cookies[:username])
-
+      @dashboard_state = 0
     elsif cookies[:usertype] == "1"
       @current_user ||= Owner.find_by(username: cookies[:username])
+      @dashboard_state = 1
     # user is both an advertiser and owner
+  elsif cookies[:usertype] == "2"
+    if cookies[:dashboard_state] == 0
+      @dashboard_state = 0
+    else
+      @dashboard_state = 1
+    end
   end
-  p 'current user is ', @current_user.username, 'and the number is', @current_user.usertype==1
 end
 rescue ActiveRecord::RecordNotFound
   session[:user_id] = nil
 end
+
+
+
+
 
 end
--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -4,14 +4,14 @@ class UserController < ApplicationController
    def new
      @user = User.new
    end
-   
+
     SUCCESS = 1
     FAILURE = -1
 
     # Create a user
     def createUser
          user = 0
-         # Depending on usertype, create an Advertiser or Owner
+         # Depending on usertype, create an Advertiser (0) or Owner (1)
          params = create_params
          type = params[:usertype]
          if type.eql? "0" or type.equal? 0
@@ -20,8 +20,8 @@ class UserController < ApplicationController
             user = Owner.createUser(params)
          #user exists and wants to add user type
          elsif type.eql? "2" or type.equal? 2
-            current_type = cookies[:usertype] 
-            #user is already an owner
+            current_type = cookies[:usertype]
+            #user is already an owner and signed up to also to be an advertiser
             if current_type.equal? 1 or current_type.eql? '1'
                owner = Owner.find_by_username(cookies[:username])
                user = Advertiser.createUser({:username => owner.username, \
@@ -30,10 +30,11 @@ class UserController < ApplicationController
                                         :usertype => 2, \
                                         :email => owner.email})
                owner.update_attributes :usertype => 2
-               user.update_attributes :password_hash => owner.password_hash, 
+               user.update_attributes :password_hash => owner.password_hash,
                                       :password_salt => owner.password_salt
 
-            #user is already an advertiser
+
+            #user is already an advertiser and signed up to also be an owner
             elsif current_type.equal? 0 or current_type.eql? '0'
                advertiser = Advertiser.find_by_username(cookies[:username])
                user = Owner.createUser({:username => advertiser.username, \
@@ -42,8 +43,9 @@ class UserController < ApplicationController
                                         :usertype => 2, \
                                         :email => advertiser.email})
                advertiser.update_attributes :usertype => 2
-               user.update_attributes :password_hash => advertiser.password_hash, 
+               user.update_attributes :password_hash => advertiser.password_hash,
                                       :password_salt => advertiser.password_salt
+
             end
          else
             #Should never reach here
@@ -52,9 +54,13 @@ class UserController < ApplicationController
 
          # If it's a string, then it was a success
          # Else, then it was a failure
+
          if not user.is_a? Integer
             cookies[ :username ] = user.username
             cookies[ :usertype ] = user.usertype
+            #set a permanent cookie to save state of dashboard
+            # save dashboard state -- users now have the ability to toggle between dashboard states
+            cookies.permanent[:dashboard_state] = user.usertype
             render :json => { status: SUCCESS }
          else
             render :json => { status: user }
