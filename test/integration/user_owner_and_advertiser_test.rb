fa4c0cbea7313ef93de56e28ac48299065ae3a72
Jason <jasonclark@berkeley.edu>
2014-11-05 14:16:34 -0800

    update integration test

--- test/integration/user_owner_and_advertiser_test.rb
+++ test/integration/user_owner_and_advertiser_test.rb
@@ -11,11 +11,7 @@ class UserOwnerAndAdvertiserTest < ActionDispatch::IntegrationTest
 									 company: 'my comp', \
            						     usertype: '1' }}
   	assert_difference "Advertiser.count()", 1 do
-  		post '/api/create_user', {user: { username: "JohnDoe1", \
-	  									 password: "password1", \
-	  									 email: "asdf@asdf.com", \
-	  									 company: 'my comp', \
-		           						 usertype: '2' }}, \
+  		post '/api/create_user', {user: { usertype: '2' }}, \
 		           				{ "HTTP_COOKIE" => "usertype=1; username=JohnDoe1;" }
   	end
   end
@@ -27,11 +23,7 @@ class UserOwnerAndAdvertiserTest < ActionDispatch::IntegrationTest
 									 company: 'my comp', \
            						     usertype: '0' }}
   	assert_difference "Owner.count()", 1 do
-  		post '/api/create_user', {user: { username: "JohnDoe2", \
-	  									 password: "password1", \
-	  									 email: "asdf@asdf.com", \
-	  									 company: 'my comp', \
-		           						 usertype: '2' }}, \
+  		post '/api/create_user', {user: { usertype: '2' }}, \
 		           				{ "HTTP_COOKIE" => "usertype=0; username=JohnDoe2;" }
   	end
   end
