00ba78899f5309803911305fc95d3cb3efdafc2c
jesca <jesca116@gmail.com>
2014-11-06 18:36:35 -0800

    fixed navbar bugs...removed code

--- spec/integration/home_spec.rb
+++ spec/integration/home_spec.rb
@@ -1,8 +1,29 @@
 require 'spec_helper'
 
-describe 'home page' do
-  it 'should have messsage' do
-    visit '/'
-    page.should have_content('Request a quote')
+describe "People" do
+  before(:all) do
+    @verification_errors = []
+
+    @browser = Selenium::Client::Driver.new(
+      :host => "localhost",
+      :port => 4444,
+      :browser => "*firefox"
+      :url => "http://localhost:3000",
+      :timeout_in_second => 90)
+
+    @browser.start
+  end
+
+  before(:each) do
+    @browser.start_new_browser_session
+  end
+
+  append_after(:each) do
+    @browser.close_current_browser_session
+    @verification_errors.should == []
+  end
+
+  it "should create a new Person with valid input" do
+    @browser.open "/""
   end
 end
