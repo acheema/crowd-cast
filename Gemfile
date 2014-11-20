99439acd4b2bdbe688f169972afb00d505468189
Jhoong Roh <jhoong.roh@gmail.com>
2014-11-20 15:17:54 -0800

    Added calendar to the listing details page. Will remove later

--- Gemfile
+++ Gemfile
@@ -22,6 +22,8 @@ gem "font-awesome-rails"
 
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
+gem 'momentjs-rails'
+gem 'fullcalendar-rails'
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
 gem 'turbolinks'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,130 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require_tree .
+//= require moment
+//= require fullcalendar
 //= require bootstrap.min
 //= require underscore
 //= require gmaps/google
+//= require_tree .
+
+  $(document).ready(function() {
+    // Deal with radio button options (choose either advertiser or owner)
+    var radio = $('input[name=signup-user]');
+    var checkedValue = 0;
+    $('.user-option').click(function() {
+      checkedValue = radio.filter(':checked').val();
+    });
+   
+    // Click signup button
+    $('#signup').click(function(e) {
+      e.preventDefault();
+   
+      $.ajax({
+         //First send a post request, get the response back. On successful message back, check the data
+         url: "" + window.location.protocol + '//' + window.location.host + "/api/create_user",
+         type: "POST",
+         /* radio button vals: 0 = advertiser, 1 = owner */
+ 
+         data: JSON.stringify( {username: $('#username-signup').val(), password: $('#password-signup').val(), email: $('#email-signup').val(), company: $('#company-signup').val(), usertype: checkedValue} ),
+         contentType: "application/json",
+         dataType: "json",
+         success: function(data){
+           signupSuccess(data);
+         },
+         failure: function(errorMsg) {
+           failure(data);
+         }
+      });
+    })
+   
+    function signupSuccess(data) {
+      var status = data.status;
+      if (status == 1) {
+        if (checkedValue == 0) {
+          window.location.href = window.location.protocol + "//" + window.location.host + '/search';
+        }
+        else {
+          window.location.href = window.location.protocol + "//" + window.location.host + '/listings/new';
+        }
+      }
+      else {
+        if (status == -1) {
+          // error message
+          $('#signup-usernametaken-e').show();
+          setTimeout(function() { // this will automatically close the alert and remove this if the users doesnt close it in 5 secs
+         
+            // hide error message after five seconds, or it will stay there forever
+            $("#signup-usernametaken-e").hide();
+         
+          }, 5000);
+        }
+        if (status == -2) {
+          // error message
+          $('#signup-username-e').show();
+          setTimeout(function() { // this will automatically close the alert and remove this if the users doesnt close it in 5 secs
+         
+            // hide error message after five seconds, or it will stay there forever
+            $("#signup-username-e").hide();
+           
+          }, 5000);
+        }
+        if (status == -3) {
+          // error message
+          $('#signup-password-e').show();
+          setTimeout(function() { // this will automatically close the alert and remove this if the users doesnt close it in 5 secs
+         
+            // hide error message after five seconds, or it will stay there forever
+            $("#signup-password-e").hide();
+         
+          }, 5000);
+        }
+        if (status == -4) {
+        // error message
+          $('#signup-email-e').show();
+          setTimeout(function() { // this will automatically close the alert and remove this if the users doesnt close it in 5 secs
+         
+            // hide error message after five seconds, or it will stay there forever
+            $("#signup-email-e").hide();
+         
+          }, 5000);
+        }
+      }
+    }
+   
+    $('#login').click(function(e) {
+      e.preventDefault();
+      $.ajax({
+        //First send a post request, get the response back. On successful message back, check the data
+        url: "" + window.location.protocol + '//' + window.location.host + "/api/login",
+        type: "POST",
+        data: JSON.stringify({username: $('#username-login').val(), password: $('#password-login').val()}),
+        contentType: "application/json",
+        dataType: "json",
+        success: function(data){
+          loginSuccess(data);
+        },
+        failure: function(errorMsg) {
+          failure(data);
+        }
+      });
+    })
+   
+    function loginSuccess(data){
+      // Wrong login credentials
+      if (data.status == -1) {
+     
+         // error message
+         $('#login-error').show();
+         setTimeout(function() { // this will automatically close the alert and remove this if the users doesnt close it in 5 secs
+       
+          // hide error message after five seconds, or it will stay there forever
+          $("#login-error").hide();
+       
+        }, 5000);
+      }
+      else if (data.status == 1) {
+        window.location.href =  window.location.protocol + '//' + window.location.host + '/search';
+      }  
+    };
+  });  
