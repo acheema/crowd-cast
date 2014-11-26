1d91563d404ac9c04e3765c8bc5ede3f42b70f8a
jesca <jesca116@gmail.com>
2014-11-25 22:17:33 -0800

    added more tests

--- test/integration/reservation_integration_test.rb
+++ test/integration/reservation_integration_test.rb
@@ -29,32 +29,32 @@ class ReservationIntegrationTest < ActionDispatch::IntegrationTest
                                                width: 1, height: 1 } }
       @advertisement_id = json_response['status']
    end
-      
+
    test 'valid reservation' do
       post '/reservations', { reservation: { listing_id: @listing_id, \
                                               advertisement_id: @advertisement_id, \
                                               dates: [ {start_date: '2014-11-12', end_date: '2014-11-13', price: 14},
-                                                       {start_date: '2014-11-13', end_date: '2014-11-14', price: 15}] } } 
+                                                       {start_date: '2014-11-13', end_date: '2014-11-14', price: 15}] } }
       assert json_response['status'] == 1
    end
 
    test 'invalid reservation' do
       post '/reservations', { reservation: { listing_id: @listing_id, \
                                               advertisement_id: @advertisement_id, \
-                                              dates: [ {start_date: '-2014-11-12', end_date: '2014-11-13', price: 14}] } } 
+                                              dates: [ {start_date: '-2014-11-12', end_date: '2014-11-13', price: 14}] } }
       assert json_response['status'] == -1
    end
-   
+
    test 'too many reservations' do
       8.times do |i|
          post '/reservations', { reservation: { listing_id: @listing_id, \
                                                  advertisement_id: @advertisement_id, \
-                                                 dates: [ {start_date: '2014-11-12', end_date: '2014-11-13', price: 14}] } } 
+                                                 dates: [ {start_date: '2014-11-12', end_date: '2014-11-13', price: 14}] } }
       end
       assert json_response['status'] == 1
       post '/reservations', { reservation: { listing_id: @listing_id, \
                                               advertisement_id: @advertisement_id, \
-                                              dates: [ {start_date: '2014-11-12', end_date: '2014-11-13', price: 14}] } } 
+                                              dates: [ {start_date: '2014-11-12', end_date: '2014-11-13', price: 14}] } }
       assert json_response['status'] == -1
       assert json_response['conflicts'].count == 1
    end
@@ -62,26 +62,26 @@ class ReservationIntegrationTest < ActionDispatch::IntegrationTest
    test 'valid get' do
       get '/api/reservations', { listing_id: @listing_id, \
                              start_date: '2014-11-12', \
-                             end_date: '2014-11-13' } 
+                             end_date: '2014-11-13' }
       assert json_response['status'] == -1
       post '/reservations', { reservation: { listing_id: @listing_id, \
                                               advertisement_id: @advertisement_id, \
-                                              dates: [ {start_date: '2014-11-04', end_date: '2014-11-13', price: 14}] } } 
+                                              dates: [ {start_date: '2014-11-04', end_date: '2014-11-13', price: 14}] } }
       post '/reservations', { reservation: { listing_id: @listing_id, \
                                               advertisement_id: @advertisement_id, \
-                                              dates: [ {start_date: '2014-11-12', end_date: '2014-11-13', price: 14}] } } 
+                                              dates: [ {start_date: '2014-11-12', end_date: '2014-11-13', price: 14}] } }
       get '/api/reservations', { listing_id: @listing_id, \
                              start_date: '2014-11-10', \
-                             end_date: '2014-11-20' } 
+                             end_date: '2014-11-20' }
       assert json_response.count == 2
-      assert json_response[0] == {"start"=>"2014-11-04", "end"=>"2014-11-13"}
-      assert json_response[1] == {"start"=>"2014-11-12", "end"=>"2014-11-13"}
-   end
-   
+      assert json_response.include?({"start"=>"2014-11-04", "end"=>"2014-11-13"})
+      assert json_response.include?({"start"=>"2014-11-12", "end"=>"2014-11-13"})
+    end
+
    test 'invalid get' do
       get '/api/reservations', { listing_id: @listing_id, \
                              start_date: '-2014-11-12', \
-                             end_date: '2014-11-13' } 
+                             end_date: '2014-11-13' }
       assert json_response['status'] == -1
    end
 end
